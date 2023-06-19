b1b5dbacbee69051bbb19f88f4967c070e59d513
HamzaGuru <107999909+HamzaGuru@users.noreply.github.com>
2023-06-19 12:27:07 +0500

    changes in login page

--- application/views/front/public/login.php
+++ application/views/front/public/login.php
@@ -236,10 +236,10 @@
                   </label>
                 </div> -->
                 <div class="form-check wow fadeInLeft" ata-wow-delay="0.6s">
-                  <input class="form-check-input mt-0" type="checkbox" id="privacy" name="is_remember" <?php if (isset($_COOKIE["phone_number"])) {
+                  <input class="form-check-input" style="margin-top: 1px;" type="checkbox" id="privacy" name="is_remember" <?php if (isset($_COOKIE["phone_number"])) {
                     echo 'checked';
                   } ?>>
-                  <label class="form-check-label " for="privacy" style="margin-right: 0.3125rem;">
+                  <label class="form-check-label " for="privacy" style="margin-right: 0.615rem;">
                     <?php echo lang('remember_me'); ?>
                   </label>
                 </div>
